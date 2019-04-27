714f515489b458bd2075c7e9e509cb11f7433231
nengua <33593245+nengua@users.noreply.github.com>
2019-04-27 19:41:43 +0800

    Update NB-wordbag.py

--- NB-wordbag.py
+++ NB-wordbag.py
@@ -116,7 +116,7 @@ def do_metrics(y_test,y_pred):
 if __name__ == "__main__":
     print "Hello spam-mail"
     print "get_features_by_wordbag"
-    #x,y=get_features_by_wordbag()
-    #x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.4, random_state = 0)
+    x,y=get_features_by_wordbag()
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.4, random_state = 0)
     do_nb_wordbag(x_train, x_test, y_train, y_test)
     do_metrics(y_test,y_pred)
