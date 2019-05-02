a894ec3a57b536f2817379b0074871388b8e9e80
nengua <33593245+nengua@users.noreply.github.com>
2019-05-02 16:11:21 +0800

    Update spam-mail-double.py

--- spam-mail-double.py
+++ spam-mail-double.py
@@ -159,7 +159,7 @@ def do_dccnn(trainX, testX, trainY, testY):
     network = global_max_pool(network)
     network = dropout(network, 0.8)
     network = fully_connected(network, 2, activation='softmax')
-    network = regression(network, optimizer='Adadelta', learning_rate=0.001,
+    network = regression(network, optimizer='Adadelta', learning_rate=0.0001,
                          loss='categorical_crossentropy', name='target')
     # Training
     model = tflearn.DNN(network, tensorboard_verbose=0)
